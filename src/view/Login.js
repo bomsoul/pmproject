f00e9aba228885f31a456550e6a7383aaa8b37a2
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2020-03-27 23:14:45 +0700

    Edit Bug

--- src/view/Login.js
+++ src/view/Login.js
@@ -149,52 +149,12 @@ class Login extends React.Component {
               </Button>
             </Segment>
             <Message>
-              New for us? <Link to='/signup'> Sign Up</Link>
+              New for us? <a href='/signup'> Sign Up</a>
             </Message>
           </Form>
         </Grid.Column>
       </Grid>
       </Router>
-      // <section className="section container">
-      //   <div className="columns is-centered">
-      //     <div className="column is-half">
-      //       <form onSubmit={this.onSubmit}>
-      //         <div className="field">
-      //           <label className="label">Email</label>
-      //           <div className="control">
-      //             <input
-      //               className="input"
-      //               type="email"
-      //               name="email"
-      //               onChange={this.onChange}
-      //             />
-      //           </div>
-      //         </div>
-
-      //         <div className="field">
-      //           <label className="label">Password</label>
-      //           <div className="control">
-      //             <input
-      //               className="input"
-      //               type="password"
-      //               name="password"
-      //               onChange={this.onChange}
-      //             />
-      //           </div>
-      //         </div>
-
-      //         <div className="field is-grouped">
-      //           <div className="control">
-      //             <button className="button is-link">Submit</button>
-      //           </div>
-      //           <div className="control">
-      //             <button className="button is-text">Cancel</button>
-      //           </div>
-      //         </div>
-      //       </form>
-      //     </div>
-      //   </div>
-      // </section>
       
     )
   }
