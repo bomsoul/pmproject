a1f7d518b48ff11f8687b4fd5bf412c7f989732a
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2020-04-05 22:47:40 +0700

    Edit Uploading UI

--- src/view/AddParent.js
+++ src/view/AddParent.js
@@ -164,6 +164,8 @@ class AddParent extends Component{
                   <div class="ui icon header">
                   <i class="file image icon"></i>
                     Attach image file in listed.
+                    <br/>
+                    Status: {this.state.fullDesc===null? <i style={{color: "red"}} className="times icon"></i>: <i style={{color: "green"}} className="check icon"></i>}
                   </div>
                   <div class="fileUpload btn btn-outline-success">
                   <span>Upload</span>
@@ -178,10 +180,6 @@ class AddParent extends Component{
               <Image fluid size='medium'src={imageURL} />
               </div>
           </div>
-
-            <div style={{ position: 'relative' }}>
-              <div style={{ position: 'absolute' }}>
-              {/* <Image src={imageURL} /> */}
               {
                   this.state.fullDesc == null ? <p></p>: 
                   
@@ -224,9 +222,6 @@ class AddParent extends Component{
                   </div>
                   }
             </div>
-            {!!drawBox ? drawBox : null}
-          </div>
-        </div>
         );
       }
 }
