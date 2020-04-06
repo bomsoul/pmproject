9ee2adc6a9665b72f41d09b6781dfa4f3ef09b62
Khan <k_h_an@hotmail.com>
2020-04-06 20:53:21 +0700

    test

--- src/api/line.js
+++ src/api/line.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 // import { Link } from 'react-router-dom';
 // import Pick from '../view/Pick.js'
 import axios from 'axios';
-// import { Divider } from 'semantic-ui-react';
+// import { render } from 'react-dom';
 class Line extends Component{
     constructor(props){
         super(props);
@@ -17,13 +17,13 @@ class Line extends Component{
     
     transfer = () =>{
        let header = {
-                'Authorization': 'Bearer {3Rs86FmcOG2lGszCFEEuQqO6eLIzoOPQwXjXY3pGHzVjYfmXvj3d5hNUWPS5c4wyN8k0a+Qfx5GULhCmBRAYmHSew7DXbgme5FJPbMYv4kUXgEXIxed6KEf9jiGlunkTnCv3cw0XjT9vFbwrAUafdgdB04t89/1O/w1cDnyilFU=}',
-
+                'Authorization': 'Bearer 3Rs86FmcOG2lGszCFEEuQqO6eLIzoOPQwXjXY3pGHzVjYfmXvj3d5hNUWPS5c4wyN8k0a+Qfx5GULhCmBRAYmHSew7DXbgme5FJPbMYv4kUXgEXIxed6KEf9jiGlunkTnCv3cw0XjT9vFbwrAUafdgdB04t89/1O/w1cDnyilFU=',
+                'Access-Control-Allow-Headers' : 'Content-Type',
                 'Access-Control-Allow-Origin' : '*',
                 'Content-Type': 'application/json',
-                 'Access-Control-Allow-Methods':'POST',
-               }
-       let data ={ 
+                'Access-Control-Allow-Methods':'POST'
+               };
+       let data = { 
                 replyToken : "0839eb0d232509b911f185ae94f6eaf5",
                 // to: " U1acf7962739e020dd8d47a2f3875e672",
                 messages : [{
@@ -32,40 +32,15 @@ class Line extends Component{
                             studentName :"r",
                             parentName: "h"
                 }]
-            }
+            };
 
-        axios.post('https://api.line.me/v2/bot/message/reply',header,data).then(function(res){
+        axios.post('https://api.line.me/v2/bot/message/reply',data,{
+            headers: header
+        }).then(function(res){
                 console.log(res);
         }).catch(function(err){
             console.log(err);
         })
-
-        // const cors = require('cors');
-
-        // axios.use(cors({ origin: true }));
-        //  axios({
-        //     method: 'get',
-        //     url: 'https://my-project-1570175172905.firebaseapp.com/',
-        //     headers : {
-        //         'Access-Control-Allow-Origin' : "*",
-        //         'Content-Type': 'application/json',
-        //         'Access-Control-Allow-Methods':'GET',
-        //         'Authorization': 'Bearer {3Rs86FmcOG2lGszCFEEuQqO6eLIzoOPQwXjXY3pGHzVjYfmXvj3d5hNUWPS5c4wyN8k0a+Qfx5GULhCmBRAYmHSew7DXbgme5FJPbMYv4kUXgEXIxed6KEf9jiGlunkTnCv3cw0XjT9vFbwrAUafdgdB04t89/1O/w1cDnyilFU=}'
-        //       },
-        //       messages: [{
-        //         studentId : "Go",
-        //         parentId : "g",
-        //         studentName :"r",
-        //         parentName:"h"
-
-        //     }],
-        //     replyToken : "0839eb0d232509b911f185ae94f6eaf5"
-        // }).then(res=>{
-        //     console.log(res.messages.studentName)
-            
-        // }).catch(err=>{
-        //     console.log(err)
-        // })
 
     }
     render(){
